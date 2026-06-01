b60d295eda4b73438ae2260c8cacd79e7da8aa7e
devcaetan0 <davi_caetano@estudante.sesisenai.org.br>
2026-06-01 09:31:18 -0300

    commit para salvar

--- index.php
+++ index.php
@@ -1,33 +1,21 @@
 <?php
-session_start();
 
-$host = "localhost";
-$user = "root";
-$pass = "root";
-$db = "sistema_simples";
-
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    die("Erro na conexão");
-} else {
-    echo ("<p>BD: Conectado</p>");
-}
+include("infra/db/connect.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $usuario = $_POST["usuario"];
     $senha = $_POST["senha"];
 
-    $sql = "SELECT * FROM usuario
-    WHERE usuario = '$usuario'
-    AND senha = '$senha'";
+    $sql = "SELECT * FROM users 
+    WHERE username = '$usuario' 
+    AND password = '$senha'";
 
     $resultado = $conn->query($sql);
 
     if ($resultado->num_rows > 0) {
-        $_SESSION['usuario'] = $usuario;
-
-        header("location: public/home.php");
+        $_SESSION["usuario"] = $usuario;
+        header("Location: public/home.php");
         exit();
     } else {
         $erro = "Usuário ou senha inválidos.";
@@ -35,7 +23,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 ?>
-
 
 <html lang="en">
 
@@ -46,29 +33,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
+    <?php
+    include("public/component/navbar.php");
+    ?>
     <h2>Login com PHP</h2>
-
     <form method="POST">
-
-        <label for="usuario">Usuário:</label>
+        <label for="usuario">Usuario:</label>
         <input type="text" name="usuario">
-
-        <br><br>
-
-        <label for="senha">Senha</label>
+        <br>
+        <br>
+        <label for="senha">Senha:</label>
         <input type="password" name="senha">
+        <br>
+        <br>
+        <?php
 
-        <br><br>
-
+        if (isset($erro)) {
+            echo $erro;
+        }
+        ?>
         <button type="submit">Entrar</button>
-
     </form>
 
-    <?php
-    if (isset($erro)) {
-        echo $erro;
-    }
-    ?>
 </body>
 
 </html>
